3177370abaa5163711b2f5a4a68c2128bb473278
Bryant Xie <bryant.xie@sap.com>
2019-04-10 07:24:37 +0000

    Update 2019.04.10 15

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,6 +12,15 @@ sap.ui.define([
 
 	return Controller.extend("sap.ui.demo.vote.controller.View1", {
 		onInit: function () {
+			
+			/*vvar oViewModel2 = new JSONModel({
+					busy : false,
+					hasUIChanges : false,
+					usernameEmpty : true,
+					order : 0
+				});
+			this.getView().setModel(oViewModel2, "externalodata");*/
+			
 			var oJSONData = {
 				busy : false,
 				order : 0
@@ -19,11 +28,12 @@ sap.ui.define([
 			var oModel = new JSONModel(oJSONData);
 			oModel.loadData("./voteSummary.json");
 			this.getView().setModel(oModel, "voteSummary");
-		
+			
 			/*var oResourceBundle = new ResourceModel({
 				bundleName: "sap.ui.demo.vote.i18n.i18n"
 			});
 			sap.ui.getCore().setModel(oResourceBundle, "i18n");*/
+			//this.modelServices();
 		},
 		
 		onRefresh : function(){
@@ -46,5 +56,24 @@ sap.ui.define([
 		_getText : function(sTextId, aArgs){
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
 		}
+		/*
+		,
+		modelServices :function(){
+	        var oModeldata = new sap.ui.model.json.JSONModel();
+	        this.getView().setModel(oModeldata, "externalodata");
+	        this.intervalHandle = setInterval(function() { 
+	            //No need to create and assign a new model each time. Just load the data. 
+	            oModeldata. loadData(""); 
+	         },  10000); //Call setInterval only once
+		},
+		
+		onExit:function() {
+		   // You should stop the interval on exit. 
+		   // You should also stop the interval if you navigate out of your view and start it again when you navigate back. 
+		   if (this.intervalHandle) {
+		      clearInterval(this.intervalHandle);
+		   }
+		} 
+		*/
 	});
 });
