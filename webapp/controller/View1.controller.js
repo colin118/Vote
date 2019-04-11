4f9830b4f582b13389a78a00e1f6e2664b46c797
Colin Sun <colin.sun@sap.com>
2019-04-11 06:48:35 +0000

    Colin App

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("vote.vote.controller.View1", {
 		onInit: function () {
+			// var oJSONData = {
+			// 	busy : false
+			// };
+			// var oModel = new JSONModel(oJSONData);
+			// var oModel = this.getView().getModel("1");
+			// this.getView().setModel(oModel, "View1");
 
+			// var oModel2 = this.getOwnerComponent().getModel("su");
+			// this.getView().setModel(oModel2, "View1");
+			
+			sap.ui.getCore().byId("View1").getModel().refresh(true);
 		}
 	});
 });
